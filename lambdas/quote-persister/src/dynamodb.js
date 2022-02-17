9142e99cd70a8f408edb7a4fbec3c9dff906a611
Leandro Santos de Lima <leandro.stlima@gmail.com>
2022-02-17 16:41:58 -0300

    Passa a usar biblioteca para mapear json e dynamo objects.

--- lambdas/quote-persister/src/dynamodb.js
+++ lambdas/quote-persister/src/dynamodb.js
@@ -1,22 +1,10 @@
 const { DynamoDBClient, PutItemCommand } = require("@aws-sdk/client-dynamodb");
+const { Marshaller } = require("@aws/dynamodb-auto-marshaller");
 
-const client = new DynamoDBClient({ endpoint: `http://${process.env.LOCALSTACK_HOSTNAME}:4566` })
 
-exports.save = ({ author, quote, movie, year }) =>
-    client.send(new PutItemCommand({
-        TableName: "Quotes",
-        Item: {
-            "Author": {
-                S: author
-            },
-            "quote": {
-                S: quote
-            },
-            "movie": {
-                S: movie
-            },
-            "year": {
-                N: year
-            }
-        }
-    }));
+const client = new DynamoDBClient({ endpoint: `http://${process.env.LOCALSTACK_HOSTNAME}:4566` });
+const marshaller = new Marshaller();
+exports.save = (quote) => client.send(new PutItemCommand({
+    TableName: "Quotes",
+    Item: marshaller.marshallItem(quote)
+}));
