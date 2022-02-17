d8d1b1dc4f5d33ccb36679d172475e759b05bce7
Leandro Santos de Lima <leandro.stlima@gmail.com>
2022-02-17 12:33:03 -0300

    Correção do log recuperando o nome da tabela.

--- lambdas/quote-persister/index.js
+++ lambdas/quote-persister/index.js
@@ -4,7 +4,7 @@ exports.handler = (event) => {
     event.Records.forEach(record => {
         const { body } = record;
         dynamodb.save(JSON.parse(body))
-            .then(data => { console.log(`Quote successfully recorded in ${data.TableName} table.`); })
+            .then(data => { console.log(`Quote successfully recorded in ${data.ConsumedCapacity?.TableName} table.`); })
             .catch(error => { console.log("Error trying to record quote to database:", error); })
     });
 }
