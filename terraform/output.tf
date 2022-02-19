f7a880e209c299661852c0b6b4a948eee07b1909
Leandro Santos de Lima <leandro.stlima@gmail.com>
2022-02-19 17:58:52 -0300

    Declaração de apigateway a partir de openapi

--- terraform/output.tf
+++ terraform/output.tf
@@ -2,15 +2,3 @@
 output "api_id" {
   value = aws_api_gateway_rest_api.quotes.id
 }
-
-output "resource_id" {
-  value = aws_api_gateway_resource.quotes.id
-}
-
-output "queue_url" {
-  value = aws_sqs_queue.quotes.url
-}
-
-output "base_url" {
-  value = "${var.defaut_endpoint}/restapis/${aws_api_gateway_rest_api.quotes.id}/${aws_api_gateway_deployment.quotes.stage_name}/_user_request_/${aws_api_gateway_resource.quotes.path_part}"
-}
--- terraform/rest-api.tf
+++ terraform/rest-api.tf
@@ -1,52 +1,19 @@
-# Declarando nossa api para acesso de frases e os métodos
 resource "aws_api_gateway_rest_api" "quotes" {
-  name        = "Quotes"
-  description = "Api para consumo e envio de frases para a aplicação."
-}
-
-resource "aws_api_gateway_resource" "quotes" {
-  rest_api_id = aws_api_gateway_rest_api.quotes.id
-  parent_id   = aws_api_gateway_rest_api.quotes.root_resource_id
-  path_part   = "quotes"
-}
-
-resource "aws_api_gateway_method" "get_quotes" {
-  rest_api_id   = aws_api_gateway_rest_api.quotes.id
-  resource_id   = aws_api_gateway_resource.quotes.id
-  http_method   = "GET"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_method" "post_quote" {
-  rest_api_id   = aws_api_gateway_rest_api.quotes.id
-  resource_id   = aws_api_gateway_resource.quotes.id
-  http_method   = "POST"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "quote_receiver" {
-  rest_api_id             = aws_api_gateway_rest_api.quotes.id
-  resource_id             = aws_api_gateway_resource.quotes.id
-  http_method             = aws_api_gateway_method.post_quote.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.quote_receiver.invoke_arn
-}
-
-resource "aws_api_gateway_integration" "quote_recover" {
-  rest_api_id             = aws_api_gateway_rest_api.quotes.id
-  resource_id             = aws_api_gateway_resource.quotes.id
-  http_method             = aws_api_gateway_method.get_quotes.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.quote_recover.invoke_arn
+  name = "Quotes API"
+  body = templatefile("./openapi.json",
+    {
+      quote_receiver = "${aws_lambda_function.quote_receiver.invoke_arn}",
+      quote_recover  = "${aws_lambda_function.quote_recover.invoke_arn}"
+    }
+  )
 }
 
 resource "aws_api_gateway_deployment" "quotes" {
-  depends_on = [
-    aws_api_gateway_integration.quote_receiver,
-  ]
-
   rest_api_id = aws_api_gateway_rest_api.quotes.id
-  stage_name  = "dev"
+}
+
+resource "aws_api_gateway_stage" "quotes" {
+  deployment_id = aws_api_gateway_deployment.quotes.id
+  rest_api_id   = aws_api_gateway_rest_api.quotes.id
+  stage_name    = "dev"
 }
